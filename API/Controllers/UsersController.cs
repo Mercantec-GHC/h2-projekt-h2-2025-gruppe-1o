f731c193e751c51cf80d63ced3f7c88e750b3251
Bmantaz <Bjarkepa@gmail.com>
2025-09-04 13:21:04 +0200

    Update UsersController.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-using BCrypt.Net; // DENNE LINJE RETTER FEJLEN
+using BCrypt.Net;
 
 namespace API.Controllers
 {
